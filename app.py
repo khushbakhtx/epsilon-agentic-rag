6371b6246e86a4bf2ee8a5912535d1512e0decf2
khushbakhtx <khushbakht.dev@gmail.com>
2025-04-22 20:15:36 +0500

    agent-pandas-all-csvs

--- app.py
+++ app.py
@@ -12,11 +12,24 @@ if not openai_api_key:
     st.error("OpenAI API key not found. Please set it in the .env file.")
     st.stop()
 
-try:
-    df = pd.read_csv("data/all_data.csv")
-except FileNotFoundError:
-    st.error("CSV file 'data/all_data.csv' not found.")
-    st.stop()
+csv_files = [
+    "data/forecast_data.csv",
+    "data/oper_expenses.csv",
+    "data/test_metamodels_metrics.csv",
+    "data/all_correlations.csv",
+    "data/all_data.csv"
+    ]
+dataframes = []
+dataframe_names = []
+
+for csv_file in csv_files:
+    try:
+        df = pd.read_csv(f"{csv_file}")
+        dataframes.append(df)
+        dataframe_names.append(os.path.basename(csv_file).replace(".csv", ""))
+    except FileNotFoundError:
+        st.error(f"CSV file '{csv_file}' not found.")
+        st.stop()
 
 llm = ChatOpenAI(
     model="gpt-4o-mini",
@@ -24,16 +37,28 @@ llm = ChatOpenAI(
     temperature=0
 )
 
-agent_executor = create_pandas_dataframe_agent(
-    llm,
-    df,
-    agent_type="tool-calling",
-    verbose=True,
-    allow_dangerous_code=True
+system_prompt = """
+Ты ИИ агент, который может выполнять команды и отвечать на вопросы о данных в CSV файлах:
+У тебя есть доступ к следующим CSV файлам:
+{dataframe_info}
+Когда отвечаешь на вопрос, используй данные из CSV файлов, но ответ должен быть один, не по всем данным а только по релевантным.
+"""
+
+dataframe_info = "\n".join(
+    f"- {name}: Contains data about {name.replace('_', ' ')}." for name, df in zip(dataframe_names, dataframes)
 )
 
-st.title("epsilon.ai agent")
-st.write("Ask questions about the data in 'all_data.csv'. Type your question below.")
+agent_executor = create_pandas_dataframe_agent(
+    llm,
+    dataframes,
+    agent_type="tool-calling",
+    verbose=True,
+    allow_dangerous_code=True,
+    prefix=system_prompt.format(dataframe_info=dataframe_info)
+)
+
+st.title("epsilon.ai Agent")
+st.write("Можно задавать вопросы о данных в all_correlations.csv, all_data.csv, forecast_data.csv, oper_expenses.csv и test_metamodels_metrics.csv")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
